0897e79816206cb0fe2a3930f23843f81a7cc6fd
andrew Centeno <andrewcen166@gmail.com>
2024-10-20 10:47:11 -0400

    switcht the port to 80

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 console.log('md')
 // listen for requests
-app.listen(3000);
+app.listen(80);
 
 app.get('/', (req, res) => {
     // res.send('<p>home page</p>');
@@ -14,3 +14,7 @@ app.get('/about', (req, res) => {
     // res.send('<p>home page</p>');
     res.sendFile('./views/about.html', { root: __dirname });
 });
+
+app.use((req,res) => {
+    res.sendFile('./views/404.html',  { root: __dirname });
+})

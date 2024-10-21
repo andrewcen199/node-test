e0ac4f44919dbf5f7056ad9a89f99f120ca12d74
andrew Centeno <andrewcen166@gmail.com>
2024-10-21 03:08:31 -0400

    added ejs files

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const express = require('express');
 // express app
 const app = express();
+
+//reigster view enginer
+app.set('/view engine', 'ejs');
+
 console.log('tester')
 // listen for requests
 app.listen(80);
 
 app.get('/', (req, res) => {
     // res.send('<p>home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index');
   });
 
 app.get('/about', (req, res) => {
     // res.send('<p>home page</p>');
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about')
 });
 
 app.use((req,res) => {
-    res.sendFile('./views/404.html',  { root: __dirname });
+    res.render('404')
 });
